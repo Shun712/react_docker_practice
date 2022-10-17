d82eb77f0887be617e8dd9a173520a2af200d81f
Shun712 <sh705vi@gmail.com>
2022-10-17 21:22:04 +0900

    フード一覧のAPIを叩く

--- frontend/app/src/containers/Foods.jsx
+++ frontend/app/src/containers/Foods.jsx
@@ -1,7 +1,18 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
+
+// apis
+import {fetchFoods} from "../apis/foods";
+
 import {useParams} from "react-router-dom";
+
 export const Foods = () => {
     const { restaurantsId } = useParams();
+    useEffect(() => {
+        fetchFoods(1)
+            .then((data) =>
+                console.log(data)
+            )
+    }, []);
     return (
         <Fragment>
             フード一覧
