cfb6fa3ce98f4a0d57550cdbc6ef505ceca5b54d
Shun712 <sh705vi@gmail.com>
2022-10-17 21:52:15 +0900

    APIのデータをreducerを通して表示

--- frontend/app/src/containers/Foods.jsx
+++ frontend/app/src/containers/Foods.jsx
@@ -1,24 +1,52 @@
-import React, {Fragment, useEffect} from "react";
+import React, {Fragment, useEffect, useReducer} from "react";
+
+// reducers
+import {
+    initialState as foodsInitialState,
+    foodsActionTypes,
+    foodsReducer,
+} from "../reducers/foods";
 
 // apis
 import {fetchFoods} from "../apis/foods";
 
+// constants
+import {REQUEST_STATE} from "../constants";
+
 import {useParams} from "react-router-dom";
 
 export const Foods = () => {
-    const { restaurantsId } = useParams();
+    const {restaurantsId} = useParams();
+    const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
     useEffect(() => {
-        fetchFoods(1)
-            .then((data) =>
-                console.log(data)
-            )
+        dispatch({type: foodsActionTypes.FETCHING});
+        fetchFoods(restaurantsId)
+            .then((data) => {
+                dispatch({
+                    type: foodsActionTypes.FETCH_SUCCESS,
+                    payload: {
+                        foods: data.foods
+                    }
+                });
+            })
     }, []);
+
     return (
         <Fragment>
-            フード一覧
-            <p>
-                restaurantIdは{restaurantsId}です。
-            </p>
+            {
+                foodsState.fetchState === REQUEST_STATE.LOADING ?
+                    <Fragment>
+                        <p>
+                            ロード中...
+                        </p>
+                    </Fragment>
+                    :
+                    foodsState.foodsList.map(food =>
+                        <div key={food.id}>
+                            {food.name}
+                        </div>
+                    )
+            }
         </Fragment>
     )
 }
